2bbc6b862a43a943cbe9dd3bfb290cb2776157e2
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2026-04-12 15:05:49 +0200

    Reuse refinement scratch in canon search

--- src/traits/algorithms/canon.rs
+++ src/traits/algorithms/canon.rs
@@ -12,11 +12,8 @@ pub use partition::{
     BacktrackableOrderedPartition, OrderedPartitionCells, PartitionBacktrackPoint, PartitionCellId,
     PartitionCellView,
 };
+pub(crate) use refine::RefinementTrace;
 pub use refine::refine_partition_to_labeled_equitable;
-pub(crate) use refine::{
-    RefinementTrace, refine_partition_to_labeled_equitable_with_trace,
-    refine_partition_to_labeled_equitable_with_trace_from_splitters,
-};
 pub use search::{
     CanonSplittingHeuristic, CanonicalLabelingOptions, CanonicalLabelingResult,
     CanonicalSearchStats, LabeledSimpleGraphCertificate, canonical_label_labeled_simple_graph,
